2bff12f91a47319af1fddb0b150078f04a7a78b1
Joy McNally <bjaerickson@gmail.com>
2016-10-10 13:40:45 -0700

    new notes as of 10/10
    also finished incorporating older notes into notes2.html

---
+++ procedures.py
@@ -0,0 +1,18 @@
+# Using procedures (aka functions)
+
+def rest_of_string(s):
+  return s[1:]
+
+print rest_of_string('audacity')
+
+# Inc Procedure Quiz
+
+# What does inc function do?
+
+def inc(n):
+  return n + 1
+
+# inc function takes a number and outputs that number plus 1
+# I was correct! (duh)
+
+
